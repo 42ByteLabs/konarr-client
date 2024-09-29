c623f42214ab9372b4030440533f10fe8c5394ea
GeekMasher <geekmasher@gmail.com>
2024-09-29 22:07:43 +0100

    feat: Add Welcome Component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Title from "@/components/Title.vue";
+
+</script>
+<template>
+    <div>
+        <Title title="Welcome to Konarr"
+            description="Konarr is a simple, easy-to-use web interface for monitoring your servers, clusters, and containers for supply chain attacks." />
+    </div>
+</template>
