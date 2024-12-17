2b82cd3d0f28786cf6502b12aaa1f1361d61d41d
GeekMasher <geekmasher@gmail.com>
2024-12-17 20:57:54 +0000

    feat: Update client to support better debug/release

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const SERVER_BASE_URL = "/api";
+/// The base URL of the server comes from the environment variable or `/api` if not set
+export const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL || "/api";
 
 let api = axios.create({
     // Setting in the vite.config.ts
